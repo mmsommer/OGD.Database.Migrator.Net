614438ccd14ebe454492c0bde736596cc409a97f
Michael Sommer <m.sommer@ogd.nl>
2012-03-20 17:25:58 +0100

    Renaming of a column in SqlServerCe now fixed

--- src/Migrator.Providers/TransformationProvider.cs
+++ src/Migrator.Providers/TransformationProvider.cs
@@ -81,11 +81,11 @@ namespace Migrator.Providers
             using (
                 IDataReader reader =
                     ExecuteQuery(
-                        String.Format("select COLUMN_NAME, IS_NULLABLE from information_schema.columns where table_name = '{0}'", table)))
+                        String.Format("select COLUMN_NAME, IS_NULLABLE, DATA_TYPE from information_schema.columns where table_name = '{0}'", table)))
             {
                 while (reader.Read())
                 {
-                    Column column = new Column(reader.GetString(0), DbType.String);
+                    Column column = new Column(reader.GetString(0), Dialect.GetDbType(reader.GetString(2)));
                     string nullableStr = reader.GetString(1);
                     bool isNullable = nullableStr == "YES";
                     column.ColumnProperty |= isNullable ? ColumnProperty.Null : ColumnProperty.NotNull;
